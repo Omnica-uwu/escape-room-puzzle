b3720932478b854cfb59a7d1afd273cd91263816
Omnica Production <omnica.production@gmail.com>
2025-06-24 04:07:41 +0800

    Updated bedroom.tmx and fixed tileset paths

--- content/tilesets/3x2.tsx
+++ content/tilesets/3x2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="3x2" tilewidth="300" tileheight="200" tilecount="2" columns="1">
- <image source="../Untitled74_20250606113643.png" width="300" height="400"/>
+ <image source="../images/3x2.png" width="300" height="400"/>
 </tileset>
